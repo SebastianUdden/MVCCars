05776e0ebb42ce1495950d9c05888c7f9988bb57
SebastianUdden <sebastian.udden@gmail.com>
2016-03-02 16:06:41 +0100

    Added _ViewImports

--- src/MVCCars/ViewModels/CreateCarViewModel.cs
+++ src/MVCCars/ViewModels/CreateCarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,18 @@ namespace MVCCars.ViewModels
 {
     public class CreateCarViewModel
     {
+        [Display(Name = "Make")]
+        [Required(ErrorMessage = "Must have brand!")]
         public string Brand { get; set; }
+
+        [Range(3, 5, ErrorMessage = "Must have 3-5 doors"]
         public int Doors { get; set; }
+
+        [Range(0, 300, ErrorMessage = "Max speed is 300!"]
         public int TopSpeed { get; set; }
+
+        [Range(true, true, ErrorMessage = "You must accept Terms & Conditions")]
+        [Display(Name = "I Accept Terms and conditions")]
         public bool AcceptTerms {get; set; }
     }
 }
--- src/MVCCars/ViewModels/ListCarViewModel.cs
+++ src/MVCCars/ViewModels/ListCarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace MVCCars.ViewModels
 {
     public class ListCarViewModel
     {
+        [Display(Name = "Car Brand")]
         public string Brand { get; set; }
 
         public bool ShowAsFast { get; set; }
